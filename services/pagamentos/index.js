b8cc498f6fbeb84c32d0f3e435cae0d147b98aa2
Jessica <jessicasanttoss.silva@gmail.com>
2026-02-14 18:20:34 -0300

    Initialize payment service with Express server

---
+++ services/pagamentos/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Serviço de Pagamentos rodando 💳");
+});
+
+app.listen(3000, () => {
+  console.log("Pagamentos rodando na porta 3000");
+});
